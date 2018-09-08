bc6316cb0b9f1ebf7465a7e329dc4f3cc8528541
Idlir Shkurti <idlir.sh@gmail.com>
2018-09-09 01:02:50 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 00:59:45 2018
+
+@author: idlirshkurti
+"""
+
+from bs4 import BeautifulSoup as bs4
+import requests
+from selenium import webdriver
+import re
+import pandas as pd
+import time
+from random import random
+import os
+from tqdm import tqdm
+
+#os.chdir('repository_path')
+from src.DataLoading import *
+
+
+def main():
+    #make predictions!
+    return(predictions)
+
+if __name__ == "__main__":
+    main()
